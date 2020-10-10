b0556991ce96827ca18c3dabf04b0213a5f865ca
Aziz Ndiaye <azibdndiaye@gmail.com>
2020-10-10 15:24:44 -0400

    initialisation de la classe MeshData

---
+++ src/mesh/meshData/meshData.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class MeshData
+{
+private:
+    // Paramètres du maillage
+    int NDIME;
+    int NELEM;
+    int NPOIN;
+    int NMARK;
+    vector<string> MARKER_TAG;
+    vector<int> MARKER_ELEMS;
+
+    // Tableaux du maillage
+    double *nodes // Coordonnées des noeuds
+        vector<double> *element2nodes;
+    int *element2nodesStart;
+    int *elementTypes;
+
+    // Frontières
+    vector<vector<double> *> *element2nodesBoundary;
+    vector<int *> *element2nodesStartBoundary;
+    vector<int *> *boundaryTypes;
+
+public:
+    MeshData();
+    ~MeshData();
+};
