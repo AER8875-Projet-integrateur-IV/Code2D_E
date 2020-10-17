6c0e4519aea86d8de0200f569fb2f73ebd812b56
Aziz Ndiaye <azibdndiaye@gmail.com>
2020-10-16 21:25:54 -0400

    Connectivité locale des faces

--- src/mesh/meshData/vtkConnectivity.hpp
+++ src/mesh/meshData/vtkConnectivity.hpp
@@ -1,5 +1,5 @@
 /* 
-/Fichier meshData.hpp
+/Fichier vtkConnectivity.hpp
 /Date: 16 octobre 2020 15h58
 /Projet: Projet Intégrateur 4
 */
@@ -13,78 +13,68 @@
 
 using namespace std;
 
-class MeshData
+class VTKConnectivity
 {
 private:
-    // Paramètres du maillage
-    int _NDIME;
-    int _NELEM;
-    int _NPOIN;
-    int _NMARK;
-    int _NFACE;
-    vector<string> _MARKER_TAG;
-    vector<int> _MARKER_ELEMS;
-
-    // Tableaux du maillage
-    vector<double> _nodes; // Coordonnées des noeuds
-    vector<int> _element2Nodes;
-    vector<int> _element2NodesStart;
-    vector<int> _elementTypes;
-    bool _nodesReserve = false;
-    bool _element2NodesReserve = false;
-
-    // Frontières
-    vector<vector<int>> _element2NodesBoundary;
-    vector<vector<int>> _element2NodesStartBoundary;
-    vector<vector<int>> _elementTypesBoundary;
-    bool _element2NodesBoundaryReserve = false;
-    vector<bool> _element2NodesBoundaryReserves;
-
-    // Connectivé
-
-    /// Connectivité node2elements
-    vector<int> _esup;
-    vector<int> _esupStart;
-    void setEsup();
-
-    /// Connectivité des faces
-    vector<int> _NFAEL;
-    vector<vector<int>> _NNOFA;
-    vector<vector<vector<int>>> _lpofa;
-    void getVTKConnectivity(int vtkIndex, vector<vector<int>> &ilpofa, int iElem);
-    void setNNOFA(int iElem, vector<vector<int>> &ilpofa);
-    void setFaces();
-    int VTK2NFAEL(const int &vtkIndex);
-
-    /// Connectivité element2elements
-    vector<int> _esuelStart;
-    vector<int> _esuel;
-    vector<int> _fsuel;
-    vector<int> _esuf;
-    void setEsuel();
+    // Nombre de faces par éléments
+    vector<int> _NFAEL{
+        0,
+        0, // VTK = 1 VERTEX
+        0,
+        0, // VTK = 3 LINE
+        0,
+        3, // TVTK = 5 TRIANGLE
+        0,
+        0,
+        4, // VTK = 8 PIXEL
+        4, // VTK = 9 QUAD
+        4, // VTK = 10 TETRA
+        6, // VTK = 11 VOXEL
+        6, // VTK = 12 HEXAHEDRON
+        5, // VTK = 13 WEDGE
+        5, // VTK = 14 PYRAMID
+    };
+    vector<vector<int>> _NNOFA{
+        {0},
+        {0}, // VTK = 1 VERTEX
+        {0},
+        {0}, // VTK = 3 LINE
+        {0},
+        {2, 2, 2}, // TVTK = 5 TRIANGLE
+        {0},
+        {0},
+        {2, 2, 2, 2},       // VTK = 8 PIXEL
+        {2, 2, 2, 2},       // VTK = 9 QUAD
+        {3, 3, 3, 3},       // VTK = 10 TETRA
+        {4, 4, 4, 4, 4, 4}, // VTK = 11 VOXEL
+        {4, 4, 4, 4, 4, 4}, // VTK = 12 HEXAHEDRON
+        {3, 4, 3, 4, 4},    // VTK = 13 WEDGE
+        {3, 3, 3, 3, 4},    // VTK = 14 PYRAMID
+    };
+    vector<vector<vector<int>>> _LPOFA{
+        {{0}},
+        {{0}}, // VTK = 1 VERTEX
+        {{0}},
+        {{0}}, // VTK = 3 LINE
+        {{0}},
+        {{0, 1}, {1, 2}, {2, 0}}, // TVTK = 5 TRIANGLE
+        {{0}},
+        {{0}},
+        {{0, 1}, {1, 3}, {3, 2}, {2, 0}},                                                     // VTK = 8 PIXEL
+        {{0, 1}, {1, 2}, {2, 3}, {3, 0}},                                                     // VTK = 9 QUAD
+        {{0, 1, 3}, {2, 3, 1}, {0, 3, 2}, {1, 2, 0}},                                         // VTK = 10 TETRA
+        {{0, 1, 5, 4}, {1, 3, 7, 5}, {7, 3, 2, 6}, {6, 2, 0, 4}, {4, 5, 7, 6}, {0, 2, 3, 1}}, // VTK = 11 VOXEL
+        {{0, 1, 5, 4}, {1, 2, 6, 5}, {6, 2, 3, 7}, {7, 3, 0, 4}, {4, 5, 6, 7}, {0, 3, 2, 1}}, // VTK = 12 HEXAHEDRON
+        {{0, 1, 2}, {2, 1, 4, 5}, {5, 4, 3}, {3, 0, 2, 5}, {0, 1, 4, 3}},                     // VTK = 13 WEDGE
+        {{0, 1, 4}, {1, 2, 4}, {4, 2, 3}, {3, 0, 4}, {0, 1, 2, 3}},                           // VTK = 14 PYRAMID
+    };
 
 public:
-    MeshData();
-    ~MeshData();
-
-    // Setters
-
-    /// Paramètres du maillage
-    void setNDIME(int NDIME);
-    void setNELEM(int NELEM);
-    void setNPOIN(int NPOIN);
-    void setNMARK(int NMARK);
-    void setMARKER_TAG(string MARKER_TAG);
-    void setMARKER_ELEMS(int MARKER_ELEMS);
-
-    /// Tableaux du maillage
-    void setNodes(vector<string> nodes);
-    void setElement2Nodes(vector<string> element2Nodes);
-    void setElement2NodesFrontieres();
-    void setElement2NodesFrontieres(vector<string> element2NodesFrontieres, int marker_index);
-
-    /// Connectivité
-    void setConnectivity();
+    VTKConnectivity();
+    ~VTKConnectivity();
 
     // Getters
+    int getNFAEL(const int &vtkIndex);
+    int getLnofa(const int &vtkIndex, const int &iFael);
+    vector<int> getLpofa(const int &vtkIndex, const int &iFael);
 };
