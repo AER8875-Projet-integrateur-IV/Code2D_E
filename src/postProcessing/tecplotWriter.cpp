2feafc31a86aad275787b0f2e6a88b36cebbd0a9
Aziz Ndiaye <azibdndiaye@gmail.com>
2020-11-08 19:59:50 -0500

    initialisation de l'écriture des fichiers avec tecplot

---
+++ src/postProcessing/tecplotWriter.cpp
@@ -0,0 +1,7 @@
+/* 
+/Fichier tecplotWriter.cpp
+/Date: 05 novembre 2020 - 14h28
+/Projet: Projet Intégrateur 4
+*/
+
+#include "./tecplotWriter.hpp"
---
+++ src/postProcessing/tecplotWriter.hpp
@@ -0,0 +1,5 @@
+/* 
+/Fichier tecplotWriter.hpp
+/Date: 05 novembre 2020 - 14h28
+/Projet: Projet Intégrateur 4
+*/
